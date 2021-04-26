f5aea55265d4974c8f25eeec59309b8d587a06bd
demiduska <andrey.demidenko39@yandex.ru>
2021-04-26 02:47:00 +0200

    add popup and php actions.php

--- src/js/index.js
+++ src/js/index.js
@@ -12,7 +12,11 @@ import './tabs';
 import Inputmask from "inputmask";
 
 $(function () {
-  const bLazy = new Blazy();
+  $(window).on('load resize', function () {
+    const bLazy = new Blazy();
+    console.log('resize');
+  });
+
   icons_load();
   $(".views__slider").slick({
     dots: false,
@@ -193,6 +197,42 @@ $(function () {
     }
   });
 
+  //ajax
+
+  $(document).on('click', '.link--show', function (e) {
+    let action = $(this).data('action');
+    let $container = $('#popup-result');
+    let data = {
+      action: action
+    }
+    $.ajax({
+      url: 'ajax/actions.php',
+      data: data,
+      type: "POST",
+      beforeSend: function (response) {
+        $container.fadeIn();
+        $container.addClass("with-loader").append("<div class='loader loader--top'>"+loaderGetCode()+"</div>");
+      },
+      success: function (response) {
+        if (response) {
+          $container.find('.popup__content-main').empty().append(response);
+          $container.removeClass("with-loader");
+          $container.find(".loader").remove();
+          console.log(response);
+        }
+      },
+      error: function (jqXHR) {
+        $container.find(".loader").remove();
+        console.log('error');
+      }
+    });
+  })
+
+  $(document).on('click','.popup-close',function (e){
+    e.preventDefault();
+    $(this).parents('.popup').fadeOut();
+  });
+
 
 });
 
@@ -221,5 +261,9 @@ function setVw() {
 
 setVw();
 window.addEventListener('resize', setVw);
+
+function loaderGetCode() {
+  return '<svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">        <path fill="#4647a8" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z" transform="rotate(243.722 25 25)">        <animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="0.6s" repeatCount="indefinite"></animateTransform>        </path>        </svg>';
+}
 
 
