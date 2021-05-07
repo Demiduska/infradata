ae20d8d0406fb86ae0ef0fb655b09e51b6c9d283
demiduska <andrey.demidenko39@yandex.ru>
2021-05-07 18:53:30 +0300

    add mobile projects

--- src/js/index.js
+++ src/js/index.js
@@ -498,6 +498,16 @@ $(function () {
   $('.ajax_form').append('<input type="text" name="org" value="" class="_org" style="visibility:hidden; height: 0; width: 0; padding: 0; border:none;"/>')
   // Антиспам х
 
+  if (typeof miniShop2 !== 'undefined'){
+    miniShop2.Callbacks.Cart.add.response.success = function() {
+      console.log('товар добавлен');
+    }
+  }
+  if (typeof pdoPage !== 'undefined') {
+    pdoPage.callbacks['after'] = function (config, response) {
+      const bLazy = new Blazy();
+    }
+  }
 
 });
 
@@ -531,9 +541,5 @@ function loaderGetCode() {
   return '<svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">        <path fill="#4647a8" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z" transform="rotate(243.722 25 25)">        <animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="0.6s" repeatCount="indefinite"></animateTransform>        </path>        </svg>';
 }
 
-if (typeof miniShop2 !== 'undefined'){
-  miniShop2.Callbacks.Cart.add.response.success = function() {
-    console.log('товар добавлен');
-  }
-}
+
 
